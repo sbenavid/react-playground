cb1b4757e0edade8adbeeb016f651d9d69f1c8ba
sbenavid <sbenavid@gmail.com>
2018-10-11 11:16:32 -0500

    empiezo a convertir todo en react components. todo hardcoded

--- src/app.js
+++ src/app.js
@@ -1,64 +1,56 @@
-console.log('App.js esta corriendo');
-
-const app = {
-  title: 'Ejemplo',
-  subtitle: 'con JSX',
-  options: []
+// React obligatoriamente pide primer letra en mayusculas
+class Header extends React.Component {
+  // react components require render
+  render() {
+    return (
+      <div>
+        <h1>Indecision</h1>
+        <h2>Vamos a ver</h2>
+      </div>
+    );
+  }
 }
 
-const onFormSubmit = (e) => {
-  e.preventDefault();
-  // obtener el valor que el usuario llenó en la forma
-  const option = e.target.elements.option.value;
-  if (option) {
-    // agrega al arreglo
-    app.options.push(option);
-    console.log('onFormSubmit', option);
-    // limpia input type
-    e.target.elements.option.value = '';
+// otra clase
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>Que debo hacer?</button>
+      </div>
+    );
   }
-  render();
-};
+}
 
-// remove all button
-// on click borrar todas las opciones.
-const onResetButton = (e) => {
-  app.options = [];
-  render();
-};
+// lista de opciones disponibles
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>jaja</p>
+      </div>
+    )
+  }
+}
 
-const onMakeDecision = () => {
-  const randomNum = Math.floor(Math.random() * app.options.length);
-  const option = app.options[randomNum];
-  alert(option);
-};
+// agregar una nueva opcion
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>Agregar uno nuevo</button>
+      </div>
+    )
+  }
+}
 
-const appRoot = document.getElementById('app');
+const jsx = (
+  <div>
+    <Header />
+    <Action />
+    <Options />
+    <AddOption />
+  </div>
+);
 
-const render = () => {
-  // JSX - Javascript XML
-  // jsx expresions deven estar encapsuladas en <div>
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'aqui tus opciones ' : 'no hay opciones'}</p>
-      <button disabled={app.options.length === 0} onClick={onMakeDecision}>Que debo hacer?</button>
-      <button onClick={onResetButton}>Quitar todos</button>
-      <ol>
-        {
-          app.options.map((texto) => {
-            return <li key={texto}>{texto}</li>
-          })
-        }
-      </ol>
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="option"/>
-        <button>Add option</button>
-      </form>
-    </div>
-  );
-  ReactDOM.render(template, appRoot);
-};
-
-render();
+ReactDOM.render(jsx, document.getElementById('app'));
