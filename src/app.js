e8ac950db350ea4dd8b908e347ae3c0ea20631ff
sbenavid <sbenavid@gmail.com>
2018-10-14 18:43:30 -0500

    Convierto a statelss functional component los metodos que no usan los estados extensivamente

--- src/app.js
+++ src/app.js
@@ -4,22 +4,40 @@ class IndecisionApp extends React.Component {
     super(props);
     this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
     this.handlePick = this.handlePick.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
     this.state = {
-      options: ['la primera', 'segunda', 'esta es la tercera']
-    }
+      options: []
+    };
   }
   // handle delete Options
   handleDeleteOptions() {
     this.setState(() => {
       return {
         options: []
-      }
-    })
+      };
+    });
   }
+  // seleccionar una opcion aleatoriamente
   handlePick() {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
     alert(option);
+  }
+  // agregar una nueva opcion
+  handleAddOption(option) {
+    // validar que no este vacio el string "option"
+    if (!option) {
+      return 'No dejar la opcion vacia';
+    } else if (this.state.options.indexOf(option) > -1 ) {
+      // ya se registro ese texto, no aceptar repetidos
+      return 'Esta opcion ya fue escrita!';
+    }
+
+    this.setState((prevState) => {
+      return {
+        options: prevState.options.concat(option)
+      };
+    });
   }
   render() {
     const title='Mi ejemplo';
@@ -36,84 +54,90 @@ class IndecisionApp extends React.Component {
           options={this.state.options}
           handleDeleteOptions={this.handleDeleteOptions}
         />
-        <AddOption />
+        <AddOption
+          handleAddOption={this.handleAddOption}
+        />
       </div>
     );
   }
 }
 
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.subtitle}</h2>
+    </div>
+  );
+}
 // React obligatoriamente pide primer letra en mayusculas
 // paso de data entre componentes a traves de props. Obtengo
 // el atributo desde la invocacion <Header title="ejemplo" />
-class Header extends React.Component {
-  // react components require render
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    );
-  }
+
+
+// convierto a stateless functional component
+const Action = (props) => {
+  return (
+    <div>
+      <button
+        onClick={props.handlePick}
+        disabled={!props.hasOptions}
+      >
+        Que debo hacer?
+      </button>
+    </div>
+  );
 }
 
-// otra clase
-class Action extends React.Component {
-  render() {
-    return (
-      <div>
-        <button
-          onClick={this.props.handlePick}
-          disabled={!this.props.hasOptions}
-        >
-          Que debo hacer?
-        </button>
-      </div>
-    );
-  }
-}
 
 // lista de opciones disponibles
-class Options extends React.Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.handleDeleteOptions}>Quitar todos</button>
-        {this.props.options.length}
-        {
-          this.props.options.map((opcion) => <OptionComponent key={opcion} optionText={opcion} />)
-        }
-        <OptionComponent />
-      </div>
-    )
-  }
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}>Quitar todos</button>
+      {props.options.length}
+      {
+        props.options.map((opcion) => <OptionComponent key={opcion} optionText={opcion} />)
+      }
+      <OptionComponent />
+    </div>
+  );
 }
 
-// una sola opcion
-class OptionComponent extends React.Component {
-
-  render() {
-    return (
-      <div>
-        {this.props.optionText}
-      </div>
-    )
-  }
+// manjea una opcion individual
+const OptionComponent = (props) => {
+  return (
+    <div>
+      {props.optionText}
+    </div>
+  );
 }
+
 
 // agregar una nueva opcion
 class AddOption extends React.Component {
+  constructor (props) {
+    super(props);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.state = {
+      error: undefined
+    };
+  }
+  // local methos
   handleAddOption(e) {
     e.preventDefault();
 
     const option = e.target.elements.option.value.trim();
-    if (option) {
-      alert(option);
-    }
+    const error = this.props.handleAddOption(option);
+
+    this.setState(() => {
+      return { error };
+    });
   }
   render() {
     return (
       <div>
+        {this.state.error && <p>{this.state.error}</p>}
         <form onSubmit={this.handleAddOption}>
           <input type="text" name="option"/>
           <button>Agregar nuevo</button>
@@ -122,5 +146,18 @@ class AddOption extends React.Component {
     )
   }
 }
+
+// stateless functional component
+// only need to return the JSX code
+// los props se pasan como argumento
+// son mas rapidas que las class component
+const User = (props) => {
+  return (
+    <div>
+      <p>Nombre: {props.name}</p>
+      <p>Edad: {props.age}</p>
+    </div>
+  )
+};
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
