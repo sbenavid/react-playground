6ef9031916d37c39b787f5e8b18236553081bcbc
sbenavid <sbenavid@gmail.com>
2018-10-15 11:25:53 -0500

    Quito comentarios y codigo que no sirve

--- src/app.js
+++ src/app.js
@@ -8,28 +8,3 @@ import IndecisionApp from './components/IndecisionApp';
 // el atributo desde la invocacion <Header title="ejemplo" />
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-class OldSyntax {
-  constructor() {
-    this.name= "Miguel";
-    this.getGreeting = this.getGreeting.bind(this);
-  }
-  getGreeting() {
-    return `Hola me llamo ${this.name}`;
-  }
-}
-
-const oldSyn = new OldSyntax();
-const getGreeting = oldSyn.getGreeting();
-console.log(getGreeting);
-
-class NewSyntax {
-  name= 'gines';
-  getGreeting = () => {
-    return `Hola me llamo ${this.name}`;
-  }
-}
-
-const nuevo = new NewSyntax();
-const saludo = nuevo.getGreeting;
-console.log(saludo());
