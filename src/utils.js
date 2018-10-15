e3ce833c280af2234dcf9aac59ff7f610af6ef8d
sbenavid <sbenavid@gmail.com>
2018-10-14 22:31:27 -0500

    uso de default methods

--- src/utils.js
+++ src/utils.js
@@ -3,5 +3,7 @@ console.log('estoy en la biblioteca de utilerias');
 const square = (x) => x * x;
 
 const add = (a,b) => a + b;
+// default method
+const substract = (a, b) => a - b;
 
-export { square, add };
+export { square, add, substract as default };
