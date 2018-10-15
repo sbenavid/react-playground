d43f46a62d8ce32c5f541c6632aa65935e3e65dc
sbenavid <sbenavid@gmail.com>
2018-10-14 22:01:36 -0500

    configuracion inicial de webpacks

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+//Entry -> output
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  }
+};
