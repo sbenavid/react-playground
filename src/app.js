5c2b97ec2f1dfd623f11b09a5c1e52ba4f3890d3
sbenavid <sbenavid@gmail.com>
2018-10-14 22:42:21 -0500

    Uso de bibliotecas de 3ros

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 //import './utils';
-import { square, add } from './utils';
+import substract, { square, add } from './utils';
 import { isAdult, canDrink } from './person';
+import validator from 'validator';
+
 
 console.log('Corriendo mi app');
 
@@ -8,4 +10,8 @@ console.log(square(5));
 
 console.log(add(100,23));
 
+console.log(substract(40,20));
+
 console.log(isAdult(29) && canDrink(29));
+console.log('----')
+console.log(validator.isEmail('sbne@gmail.com'));
