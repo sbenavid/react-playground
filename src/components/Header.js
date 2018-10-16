3f97d6e80e5390fb11bba632c95700c7aa9f9fa7
sbenavid <sbenavid@gmail.com>
2018-10-15 21:41:58 -0500

    Configurar estilos para cada componente. Separados en archivos especificos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+
+// Agrego el estilo para el encabezdo, a traves de "className"
 const Header = (props) => (
-  <div>
-    <h1>{props.title}</h1>
-    {props.subtitle && <h2>{props.subtitle}</h2>}
+  <div className="header">
+    <h1 className="header__title">{props.title}</h1>
+    {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
   </div>
 );
-
 
 // Valores default para el Header
 Header.defaultProps = {
