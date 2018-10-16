f512e6649819f1b66f57fe1914c4440b0a3a62af
sbenavid <sbenavid@gmail.com>
2018-10-15 20:33:46 -0500

    Refactoring los componentes stateless

--- src/components/OptionComponent.js
+++ src/components/OptionComponent.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
 // manjea una opcion individual
-const OptionComponent = (props) => {
-  return (
-    <div>
-      {props.optionText}
-      <button
-        onClick={(e) => {
-          props.handleDeleteOption(props.optionText);
-        }}
-      >
-        Quitar
-      </button>
-    </div>
-  );
-};
+const OptionComponent = (props) => (
+  <div>
+    {props.optionText}
+    <button
+      onClick={(e) => {
+        props.handleDeleteOption(props.optionText);
+      }}
+    >
+      Quitar
+    </button>
+  </div>
+);
 
 // usar este mecanismo cuando no es una clase
 export default OptionComponent;
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import OptionComponent from './OptionComponent';
 // lista de opciones disponibles
-const Options = (props) => {
-  return (
-    <div>
-      <button onClick={props.handleDeleteOptions}>Quitar todos</button>
-      {props.options.length === 0 && <p>Agregar una opcione para poder iniciar!</p>}
-      {
-        props.options.map((opcion) => (
-          <OptionComponent
-            key={opcion}
-            optionText={opcion}
-            handleDeleteOption={props.handleDeleteOption}
-          />
-        ))
-      }
-    </div>
-  );
-};
+const Options = (props) => (
+  <div>
+    <button onClick={props.handleDeleteOptions}>Quitar todos</button>
+    {props.options.length === 0 && <p>Agregar una opcione para poder iniciar!</p>}
+    {
+      props.options.map((opcion) => (
+        <OptionComponent
+          key={opcion}
+          optionText={opcion}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))
+    }
+  </div>
+);
 
 // usar este mecanismo cuando no es una clase
 export default Options;
